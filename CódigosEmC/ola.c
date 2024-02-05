97532a3ca9d72fbe397f460901b2fb0ff884dd24
Yan Martins <yanmartinss05@gmail.com>
2024-02-05 17:59:50 -0300

    teste

---
+++ CódigosEmC/ola.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    int x, y, res;
+    printf("Digite um valor para x: ");
+    scanf("%d", &x);
+    printf("Digite um valor para y: ");
+    scanf("%d", &y);
+
+    res = x + y;
+
+    printf("O resultado de %d + %d eh igual a %d", x, y, res);
+}
