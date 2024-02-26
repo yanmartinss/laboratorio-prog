7dffb234199f8531784162f53a0ac2aa5bd5b8ac
Yan Martins <137118093+yanmartinss@users.noreply.github.com>
2024-02-26 09:07:52 -0300

    ex004

---
+++ Lista Lab. Prog/ex004.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num;
+
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &num);
+
+    printf("Seu triplo: %d\n", num*3);
+    printf("Seu quadrado: %d\n", num*num);
+    printf("Seu meio: %d\n", num/2);
+
+    return 0;
+}
