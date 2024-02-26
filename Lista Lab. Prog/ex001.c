7384b686ebea2208688b5da8dee2dfc9d11a97c0
Yan Martins <137118093+yanmartinss@users.noreply.github.com>
2024-02-26 08:54:33 -0300

    Update ex001.c

--- Lista Lab. Prog/ex001.c
+++ Lista Lab. Prog/ex001.c
@@ -5,8 +5,19 @@ int main(){
 
     int n1,n2,n3;
 
+
     n1 = 7 + 3*6/2-1;
-    printf("%d\n",n1);
+    printf("n1 = 7 + 3*6/2-1\n\n");
+    printf("Primeiro vem a multiplicação depois a divisão e depois a soma, resultado: %d\n",n1);
+
+
+    n2 = 2%2 + 2*2-2/2;
+    printf("n2 = 2%%2 + 2*2-2/2\n\n");
+    printf("Primeiro vem a divisão, multiplicação e a %% e depois a soma e a subtração, resultado: %d\n",n2);
+
+    n3 = (3*9*(3 + (9*3/(3))));
+    printf("n3 = (3 * 9 * (3 + (9*3/ (3) ) ) )\n\n");
+    printf("Primeiro vem o 9*3, depois divide pelo 3, que soma o 3 de fora do parênteses e multiplica com o 9 e o 3 de fora dos últimos parenteses\n ");
 
     return 0;
 }
