5390b4c6e6b1d2310e0c44d4372d878abb6e7feb
Yan Martins <137118093+yanmartinss@users.noreply.github.com>
2024-02-28 08:39:51 -0300

    ex006

---
+++ Lista Lab. Prog/ex006.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+    float altura, imc;
+    char sexo;
+
+    printf("Digite sua altura em metros: ");
+    scanf("%f", &altura);
+
+    while (getchar() != '\n');
+
+    printf("Digite seu sexo [M/F]: ");
+    scanf("%c", &sexo);
+
+    if (sexo == 'M'){
+        imc = 72.7 * altura - 58;
+    }else if (sexo == 'F'){
+        imc = 62.1 * altura - 44.7;
+    }
+
+    printf("O seu peso ideal para sua altura é: %.1f\n", imc);
+
+    return 0;
+}
