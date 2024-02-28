6a6e286ca1f2f3a789c354d68741ad4dd20994f9
Yan Martins <137118093+yanmartinss@users.noreply.github.com>
2024-02-28 08:56:36 -0300

    ex010

---
+++ Lista Lab. Prog/ex010.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    float volume, comprimento, largura, altura;
+
+    printf("Descobrir o volume de uma caixa retangular\n");
+
+    printf("Digite o comprimento da caixa: ");
+    scanf("%f", &comprimento);
+
+    printf("Digite a largura da caixa: ");
+    scanf("%f", &largura);
+
+    printf("Digite a altura da caixa: ");
+    scanf("%f", &altura);
+
+    volume = comprimento * largura * altura;
+
+    printf("O volume dessa caixa retangular é: %.2fL\n", volume);
+
+    return 0;
+}
