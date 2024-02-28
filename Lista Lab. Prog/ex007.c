2a2cbb3fc92256a0b0e57b6e56138440d65bdfe5
Yan Martins <137118093+yanmartinss@users.noreply.github.com>
2024-02-28 08:42:50 -0300

    ex007

---
+++ Lista Lab. Prog/ex007.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int horas, minutos, segundos, total;
+
+    printf("Digite as horas: ");
+    scanf("%d", &horas);
+
+    printf("Digite os minutos: ");
+    scanf("%d", &minutos);
+
+    printf("Digite os segundos: ");
+    scanf("%d", &segundos);
+
+    total = horas * 3600 + minutos * 60 + segundos;
+
+    printf("O total de segundos é: %d\n", total);
+    
+    return 0;
+}
