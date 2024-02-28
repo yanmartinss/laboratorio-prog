8f46d57955f145480e80d71f150bc27383f2d950
Yan Martins <137118093+yanmartinss@users.noreply.github.com>
2024-02-28 08:47:48 -0300

    ex008

---
+++ Lista Lab. Prog/ex008.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num;
+
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &num);
+
+    printf("%d elevado ao quadrado é igual a %d\n",num, num*num);
+
+    return 0;
+}
