2a175820b84dd5b46da854ea9493d4ac6a99b143
Yan Martins <137118093+yanmartinss@users.noreply.github.com>
2024-02-28 08:28:59 -0300

    ex005

---
+++ Lista Lab. Prog/ex005.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    float valor, total;
+
+    printf("Digite o valor da conta: ");
+    scanf("%f", &valor);
+
+    total = valor + ((10*valor)/100); 
+    printf("Valor total da conta com os 10%% do garçom: R$%.2f\n", total);
+
+    return 0;
+}
