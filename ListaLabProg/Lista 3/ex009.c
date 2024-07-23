ade4c55a1ea2439f2f1293a41704ed8416a127ab
yanmartinss <yanmartinss05@gmail.com>
2024-07-23 16:09:54 -0300

    Update ex009.c

--- ListaLabProg/Lista 3/ex009.c
+++ ListaLabProg/Lista 3/ex009.c
@@ -7,7 +7,7 @@ int main() {
     printf("Digite os elementos da matriz %dx%d:\n", SIZE, SIZE);
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            printf("Matriz[%d][%d]: ", i, j);
+            printf("Matriz[%d][%d]: ", i + 1, j + 1);
             scanf("%d", &matriz[i][j]);
         }
     }
