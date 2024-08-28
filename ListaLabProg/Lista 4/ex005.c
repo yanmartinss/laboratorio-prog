4c487650ba014b7007aefc198922c76d0023ae0c
Yan Martins <137118093+yanmartinss@users.noreply.github.com>
2024-08-28 09:23:06 -0300

    Update ex005.c

--- ListaLabProg/Lista 4/ex005.c
+++ ListaLabProg/Lista 4/ex005.c
@@ -1,25 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define sizeMax 100
+
+int encontrarTamanho(char *str) {
+    int tamanho = 0;
+    
+    while (*str != '\0') {
+        tamanho++;
+        str++;
+    }
+
+    return tamanho;
+}
 
 int main() {
-    char str[100];
-    char *ptr;
-    int length = 0;
+    char *str = NULL; 
+    size_t tamanho_max = sizeMax; 
+    size_t tamanho_atual;
+    
+    str = (char*)malloc(tamanho_max * sizeof(char));
 
-    puts("Digite uma string: ");
-    fgets(str, sizeof(str), stdin);
-
-    for (ptr = str; *ptr != '\0'; ptr++) {
-        if (*ptr == '\n') {
-            *ptr = '\0';
-            break;
-        }
+    if (str == NULL) {
+        printf("Erro de alocação de memória!\n");
+        return 1;
     }
 
-    for (ptr = str; *ptr != '\0'; ptr++) {
-        length++;
+    printf("Digite uma string: ");
+    fgets(str, tamanho_max, stdin);
+
+    tamanho_atual = encontrarTamanho(str);
+    if (str[tamanho_atual] == '\n') {
+        str[tamanho_atual] = '\0';
     }
 
-    printf("O comprimento da string é: %d\n", length);
+    int tamanho = encontrarTamanho(str);
+
+    printf("O tamanho da string fornecida é: %d\n", tamanho);
+
+    free(str);
 
     return 0;
 }
