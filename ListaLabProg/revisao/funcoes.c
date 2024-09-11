f86b137532f002baa67bec7d8340ded5b8f6494a
Yan Martins <yanmartinss05@gmail.com>
2024-09-10 21:36:02 -0300

    sa

---
+++ ListaLabProg/revisao/funcoes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    void imprimaMensagem(void);
+
+    imprimaMensagem();
+
+    return 0;
+}
+
+void imprimaMensagem(void) {
+    printf("teste");
+}
